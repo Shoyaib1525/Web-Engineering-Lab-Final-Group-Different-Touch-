8d1dabad716ace02f067ca2957b08480cb6337b8
Md. Shoyaib Mahmud <76205603+Shoyaib1525@users.noreply.github.com>
2020-12-18 21:51:45 +0600

    8 No. Solution

---
+++ 8 No. Solution/lab-8.php
@@ -0,0 +1,17 @@
+<?php
+$colors = array ( "color" => array ( "a" => "Red", "b" => "Green", "c" => "White"), 
+				 "numbers" => array ( 1, 2, 3, 4, 5, 6 ),
+				 "holes" => array ( "First", 5 => "Second", "Third"));
+
+
+ foreach($colors as $key => $color){
+ 	 foreach($color as $key => $value){
+
+ 	 	if($value == 'Second' || $value == 'Red'){
+ 	 		echo $value. '<br>';
+ 	 	}
+ 	}
+ }
+
+?>
+
